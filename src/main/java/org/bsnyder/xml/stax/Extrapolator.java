3ca8d7ad2d3f72b0119781cc418f59dfd63c33ed
Bruce Snyder <bruce.snyder@hybris.com>
2015-09-03 22:05:55 -0600

    Updated to parse XML without newlines and build out tests more

--- src/main/java/org/bsnyder/xml/stax/Extrapolator.java
+++ src/main/java/org/bsnyder/xml/stax/Extrapolator.java
@@ -11,7 +11,7 @@
  */
 package org.bsnyder.xml.stax;
 
-import javanet.staxutils.IndentingXMLEventWriter;
+import com.google.common.base.Stopwatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,35 +41,75 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Extrapolator {
 
     private final static Logger LOG = LoggerFactory.getLogger(Extrapolator.class);
-    private final String dirname;
-    private final int numberOfFiles;
-    private AtomicInteger counter = new AtomicInteger();
+    private String inputDirectoryName = null;
+    private String outputDirectoryName = null;
+    private int totalNumberOfFilesToCreate = 0;
+    private int numberOfCopiesPerFile = 0;
+    private AtomicInteger fileCounter = new AtomicInteger();
 
-    public Extrapolator(String dirname, int numberOfFiles) {
-        this.dirname = dirname;
-        this.numberOfFiles = numberOfFiles;
+    public Extrapolator(String inputDirectoryName, int totalNumberOfFilesToCreate) throws FileNotFoundException {
+        this.inputDirectoryName = inputDirectoryName;
+        this.totalNumberOfFilesToCreate = totalNumberOfFilesToCreate;
+
+        handleDirectory(inputDirectoryName);
     }
 
-    public synchronized void extrapolate() throws FileNotFoundException {
-        final String[] xmlFiles = grabFilesFromDirectory(dirname);
-        for(int i = 0; i < xmlFiles.length; ++i) {
-            File oldFile = new File(xmlFiles[i]);
-            if (!oldFile.exists()) {
-                throw new FileNotFoundException(" Unable to open file '" + oldFile.getPath() + "' because it does not exist");
-            } else if (!oldFile.isFile()) {
-                throw new FileNotFoundException("'" + oldFile.getPath() + "' is not a file");
-            } else {
-                String newFileName = generateNewFileNameFromOldFileName(oldFile);
-                File newFile = new File(newFileName);
-                parseOldFileAndWriteToNewFile(oldFile, newFile);
-            }
+    void handleDirectory(String inputDirectoryName) throws FileNotFoundException {
+        File dir = new File(inputDirectoryName);
+        if (dir.exists()) {
+            this.outputDirectoryName = inputDirectoryName + "/output/";
+            createDirIfNotExists(outputDirectoryName);
+        } else {
+            throw new FileNotFoundException(inputDirectoryName + " Does not exist");
         }
     }
 
+    void createDirIfNotExists(String outputDirectoryName) {
+        File outputDir = new File(outputDirectoryName);
+        if (!outputDir.exists()) {
+            outputDir.mkdir();
+        }
+    }
+
+    public synchronized void extrapolate() throws IOException {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        final String[] xmlFiles = grabFilesFromDirectory(inputDirectoryName);
+        /*
+         * 1) Loop over all XML files in the directory
+         * 2) Divide the numberOfFilesToCreate by the xmlFiles.length to determine how many copies
+         *    of each file needs to be created
+         * 3) Loop over each file to extrapolate it that many times
+         */
+        numberOfCopiesPerFile = totalNumberOfFilesToCreate / xmlFiles.length;
+        LOG.debug("Creating {} copies of each file", numberOfCopiesPerFile);
+        for(int i = 0; i < xmlFiles.length; ++i) {
+            fileCounter.set(0);
+            for (int j = 0; j <= numberOfCopiesPerFile; ++j) {
+                if (numberOfCopiesPerFile > 0 && fileCounter.get() < numberOfCopiesPerFile) {
+                    File oldFile = new File(inputDirectoryName + xmlFiles[i]);
+                    if (!oldFile.exists()) {
+                        throw new FileNotFoundException(" Unable to open file '" + oldFile.getAbsolutePath() + "' because it does not exist");
+                    } else if (!oldFile.isFile()) {
+                        throw new FileNotFoundException("'" + oldFile.getAbsolutePath() + "' is not a file");
+                    } else {
+                        String newFileName = generateNewFileNameFromOldFileName(oldFile);
+                        File newFile = new File(outputDirectoryName + newFileName);
+                        if (!newFile.exists()) {
+                            newFile.createNewFile();
+                        }
+                        parseOldFileAndWriteToNewFile(oldFile, newFile);
+                    }
+                }
+            }
+        }
+        stopwatch.stop();
+        LOG.debug("Elapsed time: {}", stopwatch);
+    }
+
     String generateNewFileNameFromOldFileName(File oldFile) {
-        int fileNum = counter.incrementAndGet();
+        int fileNum = fileCounter.incrementAndGet();
         String newFileName = null;
-        if (fileNum < numberOfFiles) {
+        if (fileNum <= totalNumberOfFilesToCreate) {
             final String baseName = oldFile.getName().substring(0, oldFile.getName().length() - 4);
             newFileName = baseName + "_" + fileNum + ".xml";
         }
@@ -81,6 +121,9 @@ public class Extrapolator {
         String[] xmlFiles = null;
         if (dir.exists()) {
             xmlFiles = dir.list(new XmlFilesOnly());
+            if (null == xmlFiles || xmlFiles.length == 0) {
+                throw new FileNotFoundException("Unable to locate XML files in directory '" + pathToDir + "'");
+            }
         } else {
             throw new FileNotFoundException("Unable to locate directory '" + pathToDir + "'");
         }
@@ -95,6 +138,9 @@ public class Extrapolator {
      * @throws FileNotFoundException
      */
     void parseOldFileAndWriteToNewFile(File xmlFileToParse, File newFile) throws FileNotFoundException {
+
+//        LOG.debug("Parsing existing doc: {}", xmlFileToParse.getAbsolutePath());
+//        LOG.debug("Creating new doc: {}", newFile.getAbsolutePath());
 
         final String tabnamElement = "TABNAM";
         final String e1pbe1matheadElement = "E1BPE1MATHEAD";
@@ -114,7 +160,9 @@ public class Extrapolator {
             XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 
             // Create the new file and hook up a writer to it
-            writer = new IndentingXMLEventWriter(outputFactory.createXMLEventWriter(outputStream, "UTF-8"));
+            writer =  outputFactory.createXMLEventWriter(outputStream, "UTF-8");
+//            writer = new IndentingXMLEventWriter(outputFactory.createXMLEventWriter(outputStream, "UTF-8"));
+//            writer = new IndentingXMLEventWriter(outputFactory.createXMLEventWriter(System.out, "UTF-8"));
 
             while(eventReader.hasNext()) {
                 XMLEvent event = eventReader.nextEvent();
@@ -133,39 +181,45 @@ public class Extrapolator {
                             write(writer, event);
                             event = eventReader.nextEvent();
                             // Write the newline event
-                            write(writer, event);
-                            event = eventReader.nextEvent();
+//                            write(writer, event);
+//                            event = eventReader.nextEvent();
                         }
                     }
 
-                    // Is this the <E1BPE1MATHEAD> element?
-                    if (event.asStartElement().getName().getLocalPart().equals(e1pbe1matheadElement)) {
-                        // The <E1BPE1MATHEAD> element has been located now find the <MATERIAL> element
-                        // Write the <E1BPE1MATHEAD> element
-                        write(writer, event);
-                        event = eventReader.nextEvent();
-                        // Write the newline event
-                        write(writer, event);
-                        event = eventReader.nextEvent();
-                        // Is this is the <MATERIAL> element?
-                        if (event.isStartElement() &&
-                                event.asStartElement().getName().getLocalPart().equals(materialElement)) {
-                            // This *is* the <MATERIAL> element so replace the ID
-                            // Write the <MATERIAL> start element
+                    if (event.isStartElement()) {
+                        // Is this the <E1BPE1MATHEAD> element?
+                        if (event.asStartElement().getName().getLocalPart().equals(e1pbe1matheadElement)) {
+                            // The <E1BPE1MATHEAD> element has been located now find the <MATERIAL> element
+                            // Write the <E1BPE1MATHEAD> element
                             write(writer, event);
+//                            event = eventReader.nextEvent();
+                            // Write the newline event
+//                            write(writer, event);
                             event = eventReader.nextEvent();
-                            // Append new string to the <MATERIAL> element text
-                            final Characters replacementText = appendMaterialElementText(eventFactory, eventReader, event);
-                            // Write the new ID to the new file
-                            write(writer, replacementText);
-                            event = eventReader.nextEvent();
-                            // Write the <MATERIAL> end element
-                            write(writer, event);
+                            // Is this is the <MATERIAL> element?
+                            if (event.isStartElement() &&
+                                    event.asStartElement().getName().getLocalPart().equals(materialElement)) {
+                                // This *is* the <MATERIAL> element so replace the ID
+                                // Write the <MATERIAL> start element
+                                write(writer, event);
+                                event = eventReader.nextEvent();
+                                String existingId = event.asCharacters().getData();
+//                                LOG.debug("Found existing id: {}", existingId);
+                                // Append new id to the <MATERIAL> element text
+                                final String uniqueId = createNewUniqueId(existingId);
+//                                LOG.debug("Creating unique id: {}", uniqueId);
+                                final Characters characters = eventFactory.createCharacters(uniqueId);
+                                // Write the new ID to the new file
+                                write(writer, characters);
+                                event = eventReader.nextEvent();
+                                // Write the <MATERIAL> end element
+                                write(writer, event);
+                            } else {
+                                write(writer, event);
+                            }
                         } else {
                             write(writer, event);
                         }
-                    } else {
-                        write(writer, event);
                     }
                 } else {
                     write(writer, event);
@@ -191,21 +245,13 @@ public class Extrapolator {
 
     /**
      * Append the &lt;MATERIAL&gt; element text with unique string "_BATCH_" + num
-     *
-     * @param eventFactory
-     * @param event
-     * @return
-     * @throws XMLStreamException
      */
-    Characters appendMaterialElementText(XMLEventFactory eventFactory, XMLEventReader eventReader, XMLEvent event) throws XMLStreamException {
-        String origValue = event.asCharacters().getData();
-        LOG.debug("Original value: {}", origValue);
-        String newValue = origValue + "_BATCH_" + counter;
-        LOG.debug("New value: {}", newValue);
-        return eventFactory.createCharacters(newValue);
+    String createNewUniqueId(String existingId) {
+        String newValue = existingId + "_BATCH_" + fileCounter;
+        return newValue;
     }
 
-    class XmlFilesOnly implements FilenameFilter {
+    static class XmlFilesOnly implements FilenameFilter {
 
         @Override
         public boolean accept(File dir, String name) {
--- src/test/java/org/bsnyder/xml/stax/ExtrapolatorTest.java
+++ src/test/java/org/bsnyder/xml/stax/ExtrapolatorTest.java
@@ -1,6 +1,8 @@
 package org.bsnyder.xml.stax;
 
+import com.google.common.base.Stopwatch;
 import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,76 +16,138 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ExtrapolatorTest {
 
-   private final static Logger LOG = LoggerFactory.getLogger(ExtrapolatorTest.class);
+    private final static Logger LOG = LoggerFactory.getLogger(ExtrapolatorTest.class);
 
-   private Extrapolator ex;
-   private String path = getClass().getClassLoader().getResource("idocs").getPath();
+    private String inputPath = getClass().getClassLoader().getResource("idocs").getPath() + "/";
+    private String outputPath = inputPath + "/output";
+    private Extrapolator ex;
 
-   public void grabFilesFromDirectory(String pathToDir) throws Exception {
-      ex = new Extrapolator(pathToDir, 5);
-      assertNotNull(ex);
-      String[] files = ex.grabFilesFromDirectory(pathToDir);
-      assertNotNull(files);
-      assertEquals(1, files.length);
-      assertEquals("test-idoc.xml", files[0]);
-   }
+    public ExtrapolatorTest() {
+        File outputDir = new File(outputPath);
+        outputDir.mkdir();
+    }
 
-   @Test
-   public void testGrabFilesFromDirectory() throws Exception {
-      String path = getClass().getClassLoader().getResource("idocs").getPath();
-      grabFilesFromDirectory(path);
-   }
+    public void grabFilesFromDirectory(String pathToDir) throws Exception {
+        ex = new Extrapolator(pathToDir, 5);
+        assertNotNull(ex);
+        String[] files = ex.grabFilesFromDirectory(pathToDir);
+        assertNotNull(files);
+        assertEquals(3, files.length);
+    }
 
-   @Test(expected = FileNotFoundException.class)
-   public void testGrabFilesFromDirectoryWithBadDirectoryName() throws Exception {
-      grabFilesFromDirectory("foo");
-   }
+    @Test
+    public void testGrabFilesFromDirectory() throws Exception {
+        String path = getClass().getClassLoader().getResource("idocs").getPath();
+        grabFilesFromDirectory(path);
+    }
 
-   @Test
-   public void testGenerateNewFileNameFromOldFileName() throws Exception {
-//      String path = getClass().getClassLoader().getResource("idocs").getPath();
-      ex = new Extrapolator(path, 5);
-      String[] files = ex.grabFilesFromDirectory(path);
-      assertNotNull(files);
-      assertEquals(1, files.length);
-      assertEquals("test-idoc.xml", files[0]);
-      String newFileName = ex.generateNewFileNameFromOldFileName(new File(files[0]));
-      assertEquals("test-idoc_1.xml", newFileName);
-   }
+    @Test(expected = FileNotFoundException.class)
+    public void testGrabFilesFromDirectoryWithBadDirectoryName() throws Exception {
+        grabFilesFromDirectory("foo");
+    }
 
-   @Test
-   public void testParseXmlFileAndWriteToNewFile() throws Exception {
-      String testIdocName = "/test-idoc.xml";
-      String newIdocName = "/new-idoc.xml";
-      File testIdocFile = new File(path + testIdocName);
-      File newIdocFile = new File(path + newIdocName);
-      ex = new Extrapolator(path, 2);
-      ex.parseOldFileAndWriteToNewFile(testIdocFile, newIdocFile);
-      assertTrue(testIdocFile.exists());
-      checkUniqueId(newIdocFile);
-      cleanUp(newIdocFile);
-   }
+    @Test
+    public void testGenerateNewFileNameFromOldFileName() throws Exception {
+        ex = new Extrapolator(inputPath, 1);
+        String testIdocName = "test-idoc.xml";
+        File testIdocFile = new File(inputPath + "/" + testIdocName);
+        String newIdocName = "test-idoc_1.xml";
+//      assertNotNull(files);
+//      assertEquals(2, files.length);
+//      assertEquals("test-idoc.xml", files[0]);
+        String newFileName = ex.generateNewFileNameFromOldFileName(testIdocFile);
+        assertEquals(newIdocName, newFileName);
+    }
 
-   private void checkUniqueId(File file) throws Exception {
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder builder = factory.newDocumentBuilder();
-      Document doc = builder.parse(file);
-      XPathFactory xPathfactory = XPathFactory.newInstance();
-      XPath xpath = xPathfactory.newXPath();
-      String xpathExpr = "ARTMAS07/IDOC/E1BPE1MATHEAD/MATERIAL/text()";
-      XPathExpression expr = xpath.compile(xpathExpr);
-      String actualText = (String) expr.evaluate(doc, XPathConstants.STRING);
-      assertEquals("000000000003051918_BATCH_0", actualText);
-   }
+    @Test
+    public void testParseSmallXmlFileAndWriteNewFile() throws Exception {
+        String testIdocName = "test-idoc.xml";
+        String newIdocName = "new-idoc.xml";
+        parseXmlFileandWriteNewFile(testIdocName, newIdocName);
+    }
 
-   private void cleanUp(File file) {
-      FileUtils.deleteQuietly(file);
-   }
+    @Test
+    public void testParseMediumXmlFileAndWriteNewFile() throws Exception {
+        String testIdocName = "O_701_0000001352676640.xml";
+        String newIdocName = "O_701_0000001352676640_1.xml";
+        parseXmlFileandWriteNewFile(testIdocName, newIdocName);
+    }
+
+    private void parseXmlFileandWriteNewFile(String testIdocName, String newIdocName) throws Exception {
+        File testIdocFile = new File(inputPath + "/" + testIdocName);
+        File newIdocFile = new File(outputPath + "/" + newIdocName);
+        ex = new Extrapolator(inputPath, 2);
+        ex.parseOldFileAndWriteToNewFile(testIdocFile, newIdocFile);
+        assertTrue(testIdocFile.exists());
+//        LOG.debug("Asserting file {}", newIdocFile.getAbsolutePath());
+        assertTrue(newIdocFile.exists());
+//        String newFileName = ex.generateNewFileNameFromOldFileName(testIdocFile);
+        String existingId = extractId(testIdocFile);
+        String expectedUniqueId = ex.createNewUniqueId(existingId);
+        checkUniqueId(newIdocFile, expectedUniqueId);
+        cleanUp(newIdocFile);
+    }
+
+    String extractId(File file) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(file);
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        String xpathExpr = "ARTMAS07/IDOC/E1BPE1MATHEAD/MATERIAL/text()";
+        XPathExpression expr = xpath.compile(xpathExpr);
+        String actualText = (String) expr.evaluate(doc, XPathConstants.STRING);
+        stopwatch.stop();
+//        LOG.debug("Elapsed time for XPath query: {}", stopwatch);
+        return actualText;
+    }
+
+    @Test
+    public void testExtrapolate() throws Exception {
+        int numberOfIdocs = 129;
+        ex = new Extrapolator(inputPath, numberOfIdocs);
+        ex.extrapolate();
+        File dir = new File(outputPath);
+        String[] xmlFilesOutput = null;
+        if (dir.exists()) {
+            xmlFilesOutput = dir.list(new Extrapolator.XmlFilesOnly());
+        }
+        String[] xmlFilesInput = ex.grabFilesFromDirectory(inputPath);
+        int numberOfCopiesPerFile = numberOfIdocs / xmlFilesInput.length;
+        int expectedNumberOfXmlFiles = xmlFilesInput.length * numberOfCopiesPerFile;
+        assertEquals(expectedNumberOfXmlFiles, xmlFilesOutput.length);
+    }
+
+    private void checkUniqueId(File file, String expectedUniqueId) throws Exception {
+        // Not sure how this will perform with large XML files
+//        LOG.debug("Checking for unique ID in file: {} and id: {}", file.getAbsolutePath(), expectedUniqueId);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(file);
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        String xpathExpr = "ARTMAS07/IDOC/E1BPE1MATHEAD/MATERIAL/text()";
+        XPathExpression expr = xpath.compile(xpathExpr);
+        String actualText = (String) expr.evaluate(doc, XPathConstants.STRING);
+        stopwatch.stop();
+//        LOG.debug("Elapsed time for XPath query: {}", stopwatch);
+
+        assertEquals(expectedUniqueId, actualText);
+    }
+
+    private void cleanUp(File file) {
+        FileUtils.deleteQuietly(file);
+    }
 
 
 }
